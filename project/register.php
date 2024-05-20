b5981231eafe82387d5652df85a8ee8698712500
00kritish <kritishmhrzn123@gmail.com>
2024-05-20 07:40:23 +0545

    Login and landing complete

---
+++ project/register.php
@@ -0,0 +1,72 @@
+<html>
+    <head>
+        <title>register</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">School Management</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-6 offset-3">
+                <form action="">
+                    <div class ="bm-3">
+                        <lable>Name</lable>
+                        <input type="text" name="Name" class="form-control">
+                    </div>
+                    <div class ="bm-3">
+                        <lable>Email</lable>
+                        <input type="email" name="email" class="form-control">
+                    </div>
+                    <div class ="bm-3">
+                        <lable>Mobile</lable>
+                        <input type="text" name="mobile" class="form-control">
+                    </div>
+                    <div class ="bm-3">
+                        <lable>Username</lable>
+                        <input type="text" name="Uname" class="form-control">
+                    </div>
+                    <div class ="bm-3">
+                        <lable>Password</lable>
+                        <input type="password" name="password" class="form-control">
+                    </div>
+                    <div class ="bm-3">
+                        <lable>Confirm Password</lable>
+                        <input type="password" name="Cpassword" class="form-control">
+                    </div>
+</body>
+</html>

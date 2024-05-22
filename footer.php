3cbf488d53b7ed08019709f03d23b5494ca54e0f
Krishna Raj Niraula <krishna.rajniraula@scollege.edu.np>
2024-05-22 07:24:03 +0545

    09.02.2081/5/22/2024 - complete

---
+++ footer.php
@@ -0,0 +1,48 @@
+<footer class="footer bg-dark text-white">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h4>About HamroVidayala</h4>
+        <p>HamroVidayala is a leading educational institution committed to providing quality education and fostering a vibrant learning community.</p>
+      </div>
+      <div class="col-md-4">
+        <h4>Contact Us</h4>
+        <p>Email: info@hamrovidayala.com</p>
+        <p>Phone: 015910255</p>
+        <p>Address: 44600 Bagmati, Lalitpur, Nepal</p>
+      </div>
+      <div class="col-md-4">
+        <h4>Follow Us</h4>
+        <ul class="list-inline">
+          <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
+        </ul>
+      </div>
+    </div>
+
+    
+  </footer>
+  <div class="copyright">
+      Copyright © 2024 HAMROVIDYALA History Archives. All Rights Reserved.
+</div>
+    </div>
+
+<style>  
+
+footer {
+    text-align: center;
+    padding: 5px;
+    background-color: #abbaba;
+    color: #000;
+  animation: fadeIn 1s;
+  }
+  .copyright{
+    color: #blue;
+    background-color: #abbaba;
+    text-align: center;
+  }
+
+
+</style>
+  
--- index.php
+++ index.php
@@ -215,49 +215,6 @@
 </section>
 <!-- Photo Gallery section ends here -->
 
-<!-- Footer section starts here -->
-<footer class="footer bg-dark text-white">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4">
-        <h4>About HamroVidayala</h4>
-        <p>HamroVidayala is a leading educational institution committed to providing quality education and fostering a vibrant learning community.</p>
-      </div>
-      <div class="col-md-4">
-        <h4>Contact Us</h4>
-        <p>Email: info@hamrovidayala.com</p>
-        <p>Phone: 015910255</p>
-        <p>Address: 44600 Bagmati, Lalitpur, Nepal</p>
-      </div>
-      <div class="col-md-4">
-        <h4>Follow Us</h4>
-        <ul class="list-inline">
-          <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
-          <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
-          <li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</footer>
-<!-- Footer section ends here -->
- 
-<!-- Footer section css code is here-->
-<style>
-  .footer {
-    animation: fadeIn 1s;
-  }
-
-  @keyframes fadeIn {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-</style>
-
-
 </body>
 </html>
+<?php include 'footer.php' ?>
